c6941f543c4519cdfbecb812c46b198dd980a3b9
carlosmsx <carlosmsx@gmail.com>
2022-08-14 12:35:58 -0300

    agregado state para manejar objetos 'tarea' cargados desde el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,11 +3,11 @@ import ListaTareas from './ListaTareas';
 import { Form, Button } from 'react-bootstrap';
 
 const Formulario = () => {
-    const [tarea, setTarea] = useState({});
+    const [tarea, setTarea] = useState({nombre:"", descripcion:""});
 
     const submitTarea = (e)=>{
         e.preventDefault();
-        console.log("submit")
+        console.log(tarea)
     }
 
     return (
@@ -15,11 +15,17 @@ const Formulario = () => {
             <Form onSubmit={submitTarea}>
                 <Form.Group className="mb-3" controlId="formTareaNombre">
                     <Form.Label>Tarea</Form.Label>
-                    <Form.Control type="text" placeholder="Ingrese nombre para la tarea" />
+                    <Form.Control type="text" placeholder="Ingrese nombre para la tarea" 
+                        onChange={(e)=>setTarea({nombre:e.target.value.trim(), descripcion:tarea.descripcion})} 
+                        value={tarea.nombre}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formTareaDescripcion">
                     <Form.Label>Descripción</Form.Label>
-                    <Form.Control type="text" placeholder="Ingrese la descripción de la tarea" />
+                    <Form.Control type="text" placeholder="Ingrese la descripción de la tarea" 
+                        onChange={(e)=>setTarea({nombre:tarea.nombre, descripcion:e.target.value.trim()})}
+                        value={tarea.descripcion}
+                    />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
