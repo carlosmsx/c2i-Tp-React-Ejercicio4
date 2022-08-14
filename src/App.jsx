c9631c4293c05afa95d99a872bad9b5f8111e3c8
carlosmsx <carlosmsx@gmail.com>
2022-08-14 12:13:17 -0300

    agregada funcion para submit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
     return (
         <div className="container">
             <h1>Ejercicio 4</h1>
+            <h2>Administador de Tareas</h2>
             <hr/>
             <Formulario/>
         </div>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ListaTareas from './ListaTareas';
 import { Form, Button } from 'react-bootstrap';
 
 const Formulario = () => {
+    const [tarea, setTarea] = useState({});
+
+    const submitTarea = (e)=>{
+        e.preventDefault();
+        console.log("submit")
+    }
+
     return (
         <div>
-            <Form>
+            <Form onSubmit={submitTarea}>
                 <Form.Group className="mb-3" controlId="formTareaNombre">
                     <Form.Label>Tarea</Form.Label>
                     <Form.Control type="text" placeholder="Ingrese nombre para la tarea" />
-                    {/* <Form.Text className="text-muted">
-                    Texto descriptivo del campo
-                    </Form.Text> */}
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formTareaDescripcion">
                     <Form.Label>Descripción</Form.Label>
                     <Form.Control type="text" placeholder="Ingrese la descripción de la tarea" />
-                    {/* <Form.Text className="text-muted">
-                    Texto descriptivo del campo
-                    </Form.Text> */}
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
